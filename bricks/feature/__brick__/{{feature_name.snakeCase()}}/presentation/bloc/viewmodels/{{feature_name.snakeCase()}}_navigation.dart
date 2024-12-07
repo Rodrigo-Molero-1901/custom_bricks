1d30baaabc9b32685d6f70758fdc2f4203df9d8a
Rodrigo-Molero-1901 <164836354+Rodrigo-Molero-Loyalty@users.noreply.github.com>
2024-12-07 12:01:37 -0500

    Updated feature

--- bricks/feature/__brick__/{{feature_name.snakeCase()}}/presentation/bloc/viewmodels/{{feature_name.snakeCase()}}_navigation.dart
+++ bricks/feature/__brick__/{{feature_name.snakeCase()}}/presentation/bloc/viewmodels/{{feature_name.snakeCase()}}_navigation.dart
@@ -1,5 +1,5 @@
 part of '{{feature_name.snakeCase()}}_view_model.dart';
 
-sealed class {{feature_name.pascalCase()}}NavigationViewModel {}
+sealed class {{feature_name.pascalCase()}}Navigation {}
 
-class ExitNavigation extends {{feature_name.pascalCase()}}NavigationViewModel {}
+class ExitNavigation extends {{feature_name.pascalCase()}}Navigation {}
--- bricks/feature/__brick__/{{feature_name.snakeCase()}}/presentation/bloc/viewmodels/{{feature_name.snakeCase()}}_overlay.dart
+++ bricks/feature/__brick__/{{feature_name.snakeCase()}}/presentation/bloc/viewmodels/{{feature_name.snakeCase()}}_overlay.dart
@@ -1,5 +1,5 @@
 part of '{{feature_name.snakeCase()}}_view_model.dart';
 
-sealed class {{feature_name.pascalCase()}}OverlayViewModel {}
+sealed class {{feature_name.pascalCase()}}Overlay {}
 
-class ShowDialog extends {{feature_name.pascalCase()}}OverlayViewModel {}
+class ShowDialog extends {{feature_name.pascalCase()}}Overlay {}
